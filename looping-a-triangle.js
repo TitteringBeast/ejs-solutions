aa56d0d62ae759480adc0d448f017e891b0f10df
Anthony Robb <a.robb.alpha@gmail.com>
2018-05-05 02:12:40 +0100

    Solve Chapter 2 Exercise 1

---
+++ looping-a-triangle.js
@@ -0,0 +1,10 @@
+/*Eloquent Javascript, 3rd Edition;
+Chapter 2: Program Structure;
+Looping a Triangle;*/
+
+function triangle() {
+	counter = "";
+	while (counter.length < 7) {
+		counter += "#";
+	console.log(counter); }
+}
